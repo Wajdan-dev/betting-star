fcb4e058a4d756d45a0314b2cc389836323a47ab
wajdan-dev <dev.ahsanali22@gmail.com>
2025-01-25 15:16:03 +0500

    changes done

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,16 +3,25 @@ function myFunction() {
     var nav = document.getElementsByClassName('nav')[0];
 
     if (x.className === "topnav") {
-        nav.className += " responsive";
+        nav.classList.add("responsive");
+        setTimeout(function () {
+            nav.classList.add("open"); // Add 'open' after responsiveness is applied
+        }, 10); // Small timeout for smooth transition
         document.body.style.overflow = 'hidden';
     } else {
-        nav.className = "nav";
+        nav.classList.remove("open"); // Remove the 'open' class for closing animation
+        setTimeout(function () {
+            nav.classList.remove("responsive"); // Remove 'responsive' after animation ends
+        }, 300); // This should match the transition time in CSS (0.3s)
+        document.body.style.overflow = 'auto';
     }
 }
 
 function myFunctionClose() {
     var nav = document.getElementsByClassName('nav')[0];
-    nav.classList.remove("responsive");
+    nav.classList.remove("open");
+    setTimeout(function () {
+        nav.classList.remove("responsive");
+    }, 300); // Match transition duration
     document.body.style.overflow = 'auto';
-
 }
